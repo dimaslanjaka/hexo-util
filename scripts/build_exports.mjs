f2f9d4d029da579fa432a98e37c89fc6b4a9742e
dimaslanjaka <dimaslanjaka@gmail.com>
2026-02-10 10:22:39 +0700

    fix: update export paths

--- scripts/build_exports.mjs
+++ scripts/build_exports.mjs
@@ -9,29 +9,31 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
 const defaultExports = {
   '.': {
-    'import': './dist/esm/index.js',
-    'require': './dist/cjs/index.js',
-    'types': './dist/esm/index.d.ts'
+    import: './dist/index.js',
+    require: './dist/index.cjs',
+    types: './dist/index.d.ts'
   },
   './highlight_alias.json': './highlight_alias.json',
   './dist/highlight_alias.json': './highlight_alias.json'
 };
 
-fs.readdirSync(path.join(__dirname, '../lib')).forEach(file => {
+fs.readdirSync(path.join(__dirname, '../lib')).forEach((file) => {
   if (!file.startsWith('_') && !file.includes('highlight_esm')) {
     defaultExports[`./dist/${file.replace('.ts', '')}`] = {
-      'import': `./dist/esm/${file.replace('.ts', '.js')}`,
-      'require': `./dist/cjs/${file.replace('.ts', '.js')}`,
-      'types': `./dist/esm/${file.replace('.js', '.d.ts')}`
+      import: `./dist/${file.replace('.ts', '.js')}`,
+      require: `./dist/${file.replace('.ts', '.cjs')}`,
+      types: `./dist/${file.replace('.js', '.d.ts')}`
     };
   }
 });
 
 // Sort the exports to ensure consistent output
-const sortedExports = Object.keys(defaultExports).sort().reduce((obj, key) => {
-  obj[key] = defaultExports[key];
-  return obj;
-}, {});
+const sortedExports = Object.keys(defaultExports)
+  .sort()
+  .reduce((obj, key) => {
+    obj[key] = defaultExports[key];
+    return obj;
+  }, {});
 
 packageJson.exports = sortedExports;
 
